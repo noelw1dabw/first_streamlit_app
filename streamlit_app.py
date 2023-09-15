c767f5c5519fcab0a9d9ce21b44e3f961135a422
noelw1dabw <145101139+noelw1dabw@users.noreply.github.com>
2023-09-15 08:40:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 
